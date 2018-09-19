a819aea168a0c1335375e02560f503ac75b3794c
Xudong Liu <xudong.liu@techcul.com>
2018-09-19 19:58:49 +0800

    Initial Commit

---
+++ CTutorial/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CTutorial
+//
+//  Created by techcul_iOS on 2018/9/19.
+//  Copyright © 2018年 techcul. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
